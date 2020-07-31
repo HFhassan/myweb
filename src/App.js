b69f6c168e1ba055d18151c804d556235cda94ad
Hassan Farooqui <50511629+HFhassan@users.noreply.github.com>
2020-08-01 01:01:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import './App.css';
 function App() {
   return (
     <div>
+      <ul>
+        <li>I</li>
+        <li>am</li>
+        <li>nothing</li>
+        <li>but A Muslim</li>
+        <li>{7+8+6}</li>
+      </ul>
       <h1>Hello World</h1>
       <p>to the world</p>
+      <p><strong>Hassan Farooqui</strong></p>
     </div>
   );
 }
