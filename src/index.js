a1e4d29b7e390a03206ea13582544f79dd769c99
Hassan Farooqui <50511629+HFhassan@users.noreply.github.com>
2020-08-01 14:35:18 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import Dinner from './Dinner';
 import * as serviceWorker from './serviceWorker';
 
