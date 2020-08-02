8bd4e9b3640aeead53529591b65a7a2402893e72
Hassan Farooqui <50511629+HFhassan@users.noreply.github.com>
2020-08-03 02:16:16 +0500

    using css classes conditionally

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 
 function App() {
   let [count,setCount] = useState(0);
-  let [green,blue] = useState(true);
+  let [green,setGreen] = useState(true);
   return (
-    <div className="box">
+    <div className= {green ? 'box' : 'colorChange'}>
       <h3>background color is {green ? 'green':'blue'}</h3>
       <h4>you have clicked me {count} times</h4>
       <button
         onClick = {()=> setCount(count+1)}>
         click me 
+      </button>
+      <button onClick = { () => { setGreen(!green)}}
+      >
+        change color
       </button>
     </div>
   );
