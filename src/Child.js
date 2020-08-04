c938908dc8d38a0284d2a7a8c7cb3abdf2a538ba
Hassan Farooqui <50511629+HFhassan@users.noreply.github.com>
2020-08-04 08:15:22 +0500

    created increment button

--- src/Child.js
+++ src/Child.js
@@ -4,12 +4,16 @@ import counterContext from './CounterContext';
 
 const  Child = (props) =>  {
     let counterValue = useContext (counterContext);
-    console.log(counterValue);
+    
     return (
         <div>
             <h1>This is first Child</h1>
             <h3>My name is {props.name}</h3>
-            <h5>counter value is {counterValue}</h5>
+            <h5>counter value is {counterValue[0]}</h5>
+            <button 
+                onClick = {()=> {counterValue[1](++counterValue[0])}}>
+                Press to Increment    
+            </button>
         </div>
     )
 
--- src/Parent.js
+++ src/Parent.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Child from './Child';
 import counterContext from './CounterContext';
 
 const Parent = (props) => {
+    let countState = useState(10)
+    //countState is an array [count, setCount]
     return (
-        <counterContext.Provider value={60}>
+        <counterContext.Provider value={countState}>
             <Child name = {props.name} />
         </counterContext.Provider>
     )
